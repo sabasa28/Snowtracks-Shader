bc34cba7b4768ebefdb155451a2066ca3654af2e
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2021-11-24 23:03:00 -0300

    Achived first version of shader, kinda scuffed but cool

--- Assets/Scripts/DrawWithMouse.cs
+++ Assets/Scripts/DrawWithMouse.cs
@@ -6,12 +6,26 @@ public class DrawWithMouse : MonoBehaviour
 {
     [SerializeField] Camera _camera;
     [SerializeField] Shader drawShader;
+    [SerializeField] Texture2D footprintTexUp;
+    [SerializeField] Texture2D footprintTexDown;
+
+    enum dir
+    {
+        up,
+        left,
+        right,
+        down
+    }
+    bool rightFoot;
+    dir stepingDirection;
+    Vector2 lastStep = new Vector2(0 , 0);
+    Vector2 movementVec = new Vector2(0, 0);
 
     private RenderTexture splatmap;
     private Material trackableMaterial;
     private Material drawMaterial;
     RaycastHit hit;
-
+    Vector3 mouseLastPos = new Vector2(0,0);
     void Start()
     {
         drawMaterial = new Material(drawShader);
@@ -20,26 +34,67 @@ public class DrawWithMouse : MonoBehaviour
         trackableMaterial = GetComponent<MeshRenderer>().material;
         splatmap = new RenderTexture(1024, 1024, 8, RenderTextureFormat.ARGBFloat);
         trackableMaterial.SetTexture("_Splat", splatmap);
+        drawMaterial.SetTexture("_FootTexU", footprintTexUp);
+        drawMaterial.SetTexture("_FootTexD", footprintTexDown);
+        drawMaterial.SetInt("_DividedIn", 10);
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out hit, 100.0f, (1 << LayerMask.NameToLayer("Trackable"))))
         {
-            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out hit))
+            movementVec = new Vector2(hit.textureCoord.x, hit.textureCoord.y) - lastStep;
+            lastStep = new Vector2(hit.textureCoord.x, hit.textureCoord.y);
+            Debug.Log(movementVec.normalized);
+            //Debug.Log(new Vector2(hit.textureCoord.x, hit.textureCoord.y));
+            drawMaterial.SetVector("_Coordinate", new Vector4(hit.textureCoord.x, hit.textureCoord.y, 0, 0));
+            drawMaterial.SetInt("_FlipFoot", rightFoot ? 1 : 0);
+            float angleOfStep = Vector2.Angle(Vector2.up, movementVec.normalized);
+            if (movementVec.x < 0) angleOfStep = -angleOfStep;
+            Debug.Log(angleOfStep);
+            drawMaterial.SetFloat("_StepRot", angleOfStep);
+            rightFoot = !rightFoot;
+            RenderTexture temp = RenderTexture.GetTemporary(splatmap.width, splatmap.height, 0, RenderTextureFormat.ARGBFloat);
+            Graphics.Blit(splatmap, temp);
+            Graphics.Blit(temp, splatmap, drawMaterial);
+            RenderTexture.ReleaseTemporary(temp);
+        }
+        if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out hit, 100.0f, (1 << LayerMask.NameToLayer("Trackable"))))
+        { 
+            //mouseLastPos = last hit.textureCoord
+        }
+    }
+
+    void SetStepingDirection(Vector2 direction)
+    {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if (direction.x > 0)
             {
-                drawMaterial.SetVector("_Coordinate", new Vector4(hit.textureCoord.x, hit.textureCoord.y, 0, 0));
-                RenderTexture temp = RenderTexture.GetTemporary(splatmap.width, splatmap.height, 0, RenderTextureFormat.ARGBFloat);
-                Graphics.Blit(splatmap, temp);
-                Graphics.Blit(temp, splatmap, drawMaterial);
-                RenderTexture.ReleaseTemporary(temp);
+                stepingDirection = dir.right;
+            }
+            else
+            {
+                stepingDirection = dir.left;
+            }
+        }
+        else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if (direction.y > 0)
+            {
+                stepingDirection = dir.up;
+            }
+            else
+            {
+                stepingDirection = dir.down;
             }
         }
     }
 
     private void OnGUI()
     {
-        GUI.DrawTexture(new Rect(0, 0, 256, 256), splatmap, ScaleMode.ScaleToFit, false, 1);
+        GUI.DrawTexture(new Rect(0, 0, 256/2, 256/2), splatmap, ScaleMode.ScaleToFit, false, 1);
         
     }
 }
--- Assets/Scripts/Shaders/DrawTracks.shader
+++ Assets/Scripts/Shaders/DrawTracks.shader
@@ -3,9 +3,14 @@ Shader "Unlit/DrawTracks"
     Properties
     {
         _MainTex ("Texture", 2D) = "white" {}
+        _DividedIn("DividedIn", Int) = 10
+        _FootTexU ("FootTextureU", 2D) = "white" {}
+        _FootTexD ("FootTextureD", 2D) = "white" {}
+        _FootSize ("FootSize", Int) = 0
+        _FlipFoot("FlipFoot", Int) = 0
+        _StepRot("StepRot", Float) = 0.0
         _Coordinate("Coordinate", Vector) = (0,0,0,0)
         _Color("Draw Color", Color) = (1,0,0,0)
-        _FootprintTex("Footprint Texture", 2D) = "white" {}
         //_PencilThickness("Pencil Thickness", Range (0, 200)) = 50.0
     }
     SubShader
@@ -34,10 +39,14 @@ Shader "Unlit/DrawTracks"
             };
 
             sampler2D _MainTex;
+            sampler2D _FootTexU;
+            sampler2D _FootTexD;
+            float _StepRot;
+            int _FlipFoot;
+            int _DividedIn;
             float4 _MainTex_ST;
             fixed4 _Coordinate;
             fixed4 _Color;
-            //float _PencilThickness;
 
             v2f vert (appdata v)
             {
@@ -47,15 +56,60 @@ Shader "Unlit/DrawTracks"
                 return o;
             }
 
-            fixed4 frag (v2f i) : SV_Target
-            {
-                //_PencilThickness - 
+            float2 rotateUV(float2 uv, float2 pivot, float rotation) {
+                float sine = sin(rotation);
+                float cosine = cos(rotation);
+                float aux;
+                
+                uv -= pivot;
+                aux = uv.x;
+                uv.x = uv.x * cosine - uv.y * sine;
+                uv.y = aux * sine + uv.y * cosine;
+                uv += pivot;
 
-                fixed4 col = tex2D(_MainTex, i.uv);
-                float draw = pow(saturate(1 - distance(i.uv, _Coordinate.xy)), 200); //aca hacer un clamp y hacer algo como pencilthickness - 200 o algo
-                fixed4 drawcol = _Color * (draw * 0.5); // float = growspeed
-                return saturate(col + drawcol);
+                return uv;
             }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                float2 coordsToLimit = float2(round(_Coordinate.x * 10) / 10, round(_Coordinate.y * 10) / 10);
+                float2 coordsToDraw = _Coordinate.xy;
+                float2 uv = i.uv;
+                //if (_FlipFoot == 0) float2 (1.0 - i.uv.x, i.uv.y);
+                uv = rotateUV(uv, coordsToDraw, _StepRot * 0.01745329251);
+                float2 uvToShow = uv;
+                fixed4 used;
+                fixed4 foot;
+                if (_FlipFoot == 0) foot = tex2D(_FootTexU, uv * _DividedIn);
+                else foot = tex2D(_FootTexD, uv * _DividedIn);
+                fixed4 col = tex2D(_MainTex, i.uv);
+                float2 limit1 = coordsToLimit;
+                float2 limit2 = coordsToLimit + float2(1.0 / _DividedIn, 1.0 / _DividedIn);
+                if (uvToShow.x > limit1.x && uvToShow.y > limit1.y && uvToShow.x < limit2.x && uvToShow.y < limit2.y)
+                        used = foot;
+                    else
+                        used = col;
+
+                //if (_FlipFoot == 0)
+                //{
+                //    if (uvToShow.x < limit.x && uvToShow.y < limit.y)
+                //        used = foot;
+                //    else
+                //        used = col;
+                //}
+                //else
+                //{
+                //    if (uvToShow.x > limit.x && uvToShow.y < limit.y)
+                //        used = foot;
+                //    else
+                //        used = col;
+                //}
+
+                float draw = pow(saturate(1 - distance(i.uv, _Coordinate.xy)), 200);
+                fixed4 drawcol = _Color * (draw * 0.5);
+                return saturate(used);
+            }
+
             ENDCG
         }
     }
